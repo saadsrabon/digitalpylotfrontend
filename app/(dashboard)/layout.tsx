42f04e4a7f7247813923a54453daa1f12a5714b2
saadsrabon <saadsrabon2@gmail.com>
2026-03-15 02:29:10 +0600

    feat(dashboard) dashboard demo pages added

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,19 +1,21 @@
 import Navbar from "@/components/layouts/navbar";
 import Sidebar from "@/components/layouts/sidebar";
 
-
 export default function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex">
+    <div className="flex min-h-screen">
       <Sidebar />
 
-      <div className="flex-1">
+      <div className="flex flex-col flex-1">
         <Navbar />
-        <main className="p-6">{children}</main>
+
+        <main className="flex-1 p-6 bg-gray-50">
+          {children}
+        </main>
       </div>
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div></div>
+    <div>
+      <h2>Hello</h2>
+    </div>
   );
 }
